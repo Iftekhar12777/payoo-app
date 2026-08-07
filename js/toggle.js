dab83d12aa43a59cb70d1bf0ebfb4f73d221203f
Iftekhar Uddin <iftekhar@Iftekhars-MacBook-Air.local>
2026-08-07 18:53:28 +0600

    menu buttn acitve effect added

--- js/toggle.js
+++ js/toggle.js
@@ -6,11 +6,25 @@ for(const moneySection of moneySections) {
 document.getElementById(sectionToShow).style.display = "block";
 }
 
+function activeColor(id) {
+    const menuBtns = document.getElementsByClassName("menuBtn");
+    for(const menuBtn of menuBtns) {
+        menuBtn.classList.remove("bg-[#0874f233]","border-2", "border-[#0874f2]", "text-[#0874f2]");
+        menuBtn.classList.add("border-gray-200");
+    }
+    document.getElementById(id).classList.remove("border-gray-200");
+    document.getElementById(id).classList.add("bg-[#0874f233]","border-2", "border-[#0874f2]", "text-[#0874f2]");
+}
 
+
+
+// implementing the toggle functionality for the Add Money and Cash Out sections
 document.getElementById("togAddMoney").addEventListener("click", function () {
     toggleSections("addMoneySection");
+    activeColor("togAddMoney");
 });
 
 document.getElementById("togCashOut").addEventListener("click", function () {
     toggleSections("cashOutSection");
+    activeColor("togCashOut");
 });
